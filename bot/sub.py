4b7ee26b6a396bf10c4a95dca2002925cc75bd2b
2gisgit <100689917+2gisgit@users.noreply.github.com>
2022-07-01 23:35:38 +0900

    믕

--- bot/sub.py
+++ bot/sub.py
@@ -1,7 +1,9 @@
 import os
 import time
+
 def run(code, filename):
   time.sleep(2)
-  f = open(filename, "w", encoding="utf8")
+  f = open(filename, 'w', encoding='utf8')
   f.write(code)
-  os.system(f"python {filename}")
+  os.system(f'python {filename}')
+  
